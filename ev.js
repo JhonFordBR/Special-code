d94cf6e11a3d8b3480c41229d5259ed333496494
johnford <johnford@localhost.localdomain>
2017-05-18 21:09:11 +0400

    to many ideas

--- ev.js
+++ ev.js
@@ -1,2 +1,35 @@
 'use strict';
-setInterval(function(){console.log('iteration')},3000)
+/*let counter = 0;
+
+const interval = setInterval(function(){
+
+  console.log(`iteration ${++counter}`)
+  if(counter===7){clearInterval(interval);}
+},3000);
+*/
+//process.stdin.on('data',function(data){
+//console.log(data.toString());
+
+
+//});
+const server = require('http').createServer((req,res)=>{
+
+if(req.url=='/'){
+  res.setHeader('content-type','text/html');
+  res.end(
+    `<!Doctype html>
+    <body>
+    <h1>WATAFAK===error</h1>
+    <p>watafak</p>
+    </body>`
+
+
+
+
+  )
+}else{
+  res.end(`UNKNOWN`)
+
+}
+
+}).listen(8080,()=>console.log('Started our server'));
